172fba68dbbdb2c95578fa7fce8f3b979f5fcabc
Oliver <oliver.s.singh@gmail.com>
2024-12-25 18:13:08 +0000

    added function to extract table

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -24,5 +24,17 @@ def get_page(username, page):
     else:
         raise Exception(HTTPError, f"HTTP error, status code {response.status_code},response reason '{response.reason}'")
 
+def extract_table(soup):
+    data = []
+    table = soup.find('div', attrs={'id':'myLogbookTable'})
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [item.text.strip() for item in cols]
+        data.append(cols)
+    return pandas.DataFrame(data)
+
 if __name__ == '__main__':
-    get_page(297086, 1)
+    page = get_page(297086, 1)
+    extract_table(page)
